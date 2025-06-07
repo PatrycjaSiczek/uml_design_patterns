3df191b00c7964064473dd9b4490043a1fa24304
patry <patrycjasiczek5@gmail.com>
2025-06-07 11:31:20 +0200

    IO

---
+++ Iterator/src/main/java/org/example/Ksiazka.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+
+    public Ksiazka(String tytul, String autor) {
+        this.tytul = tytul;
+        this.autor = autor;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + tytul + "\" autorstwa " + autor;
+    }
+}
